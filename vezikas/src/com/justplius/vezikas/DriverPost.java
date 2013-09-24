d2c9d306c2a7aa314a9eee2b25691084c7df7995
Armandas Motuzas <armandas.motuzas@gmail.com>
2013-09-25 00:05:23 +0300

    eccfeefcfec

--- vezikas/src/com/justplius/vezikas/DriverPost.java
+++ vezikas/src/com/justplius/vezikas/DriverPost.java
@@ -6,7 +6,7 @@ import android.content.Context;
 import android.text.format.Time;
 
 public class DriverPost {
-	//ekstremalus pokyciai
+	//ekstremalus pokyciai 2
 	//Post data
 	private float rating;	
 	private int seats_available;
